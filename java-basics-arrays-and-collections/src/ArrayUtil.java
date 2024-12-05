3eb1c108d3054d3430cf7412554076481011f64b
Ihor Taranenko <ihor.taranenko@Ihor.Taranenko>
2024-12-05 16:32:06 +0200

    Added finder and matcher of elements in arrays. Added 2 unit tests.

---
+++ java-basics-arrays-and-collections/src/ArrayUtil.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class ArrayUtil {
+
+    /**
+     * Finds the common elements between two arrays (String values).
+     *
+     * @param array1 first array
+     * @param array2 second array
+     * @return String array with common elements
+     */
+    public static String[] findCommon(String[] array1, String[] array2) {
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < array1.length; i++) {
+            for (int j = 0; j < array2.length; j++) {
+                if (array1[i] == array2[j]) {
+                    set.add(array1[i]);
+                    break;
+                }
+            }
+        }
+        return set.toArray(new String[0]);
+    }
+}
---
+++ java-basics-arrays-and-collections/src/ArraysTest.java
@@ -0,0 +1,45 @@
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class ArraysTest {
+
+    /**
+     * Test data for common array elements.
+     *
+     * @return test data array
+     */
+    @DataProvider(name = "ArrayUtil")
+    public static Object[][] arrays() {
+        return new Object[][]{
+                {
+                        new String[]{"Python", "JAVA", "PHP", "C#", "C++", "SQL"},
+                        new String[]{"MySQL", "SQL", "SQLite", "Oracle", "PostgreSQL", "DB2", "JAVA"},
+                        new String[]{"JAVA", "SQL"}
+                },
+                {
+                        new String[]{"here", "is", "some", "text", ".", "text"},
+                        new String[]{"some", "text", "above", "."},
+                        new String[]{"some", "text", "."}
+                },
+                {
+                        new String[]{"one", "two", "three", "four", "five", "."},
+                        new String[]{"two", "three", "seven", "."},
+                        new String[]{"two", "three", "."}
+                },
+                {
+                        new String[]{"Honda", "Jeep", "Subaru", "Ford", "Fiat", "Nissan"},
+                        new String[]{"Honda", "Toyota", "Seat", "Tesla"},
+                        new String[]{"Honda"}
+                }
+        };
+    }
+
+    @Test(dataProvider = "ArrayUtil")
+    public void testFindCommon(String[] array1, String[] array2, String[] expectedResult) {
+
+        assertEquals(ArrayUtil.findCommon(array1, array2), expectedResult,
+                "Common elements are not correct");
+    }
+}
